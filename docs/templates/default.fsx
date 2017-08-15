cd3fd864acd649628ba1ad80c651c96c647c20ba
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-08-15 23:42:41 +0200

    Default page, get components lists

--- docs/templates/default.fsx
+++ docs/templates/default.fsx
@@ -1,9 +1,12 @@
 #r "../_bin/Fornax.Core.dll"
 #load "../siteModel.fsx"
+#load "../utils/ContentParser.fsx"
 
 
 open Html
 open SiteModel
+open System.IO
+open System.Collections.Generic
 
 let javascript =
     """
@@ -21,7 +24,40 @@ hljs.initHighlightingOnLoad();
 //     document.getElementById("tab-home").classList.add("is-active");
     """
 
+let projectRoot = Path.Combine(__SOURCE_DIRECTORY__, "..")
+
+let getComponents () =
+    let path = Path.Combine(projectRoot, "components")
+    Directory.GetFiles path
+    |> Array.filter (fun n -> n.EndsWith ".md")
+    |> Array.toList
+
+type ComponentInfo =
+    { Link : string
+      Title : string }
+
 let defaultPage (siteModel : SiteModel) pageTitle content =
+    let components = 
+        getComponents ()
+        |> List.map (fun p -> 
+                        let info : Dictionary<string, string> = new Dictionary<string, string>()
+                        let content = File.ReadAllText p
+                        let config = ContentParser.getConfig content
+                        config.Split '\n'
+                        |> Array.iter( fun l ->
+                            let kv = l.Split ':'
+                            info.Add(kv.[0], kv.[1])
+                        )
+                        let title = ref ""
+                        
+                        if info.TryGetValue("title", title) then
+                            { Link = p.Replace(projectRoot, "")
+                                      .Replace("md", "html")
+                              Title = !title }
+                        else
+                            failwith "\n\n Component need to have a title set in their config header \n\n"
+                    )
+
     html [] [
         head [] [
             meta [CharSet "utf-8"]
---
+++ docs/utils/ContentParser.fsx
@@ -0,0 +1,15 @@
+open System
+
+let private isSeparator (input : string) =
+    input.StartsWith "---"
+
+///`fileContent` - content of page to parse. Usually whole content of `.md` file
+///returns content of config that should be used for the page
+let getConfig (fileContent : string) =
+    let fileContent = fileContent.Split '\n'
+    let fileContent = fileContent |> Array.skip 1 //First line must be ---
+    let indexOfSeperator = fileContent |> Array.findIndex isSeparator
+    fileContent
+    |> Array.splitAt indexOfSeperator
+    |> fst
+    |> String.concat "\n"
