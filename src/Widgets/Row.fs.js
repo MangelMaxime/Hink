dbf1154aa226576e9eded05823d219b8b780317f
Maxime <mangel.maxime@protonmail.com>
2020-11-04 19:59:01 +0100

    Remove generated/copied file by Fable 3

--- src/Widgets/Row.fs.js
+++
@@ -1,7 +0,0 @@
-import { Row__get_ActiveRatio, Row } from "../Hink.fs.js";
-
-export function Hink_Gui_Hink__Hink_Row_Z33A93963(this$, ratios) {
-    this$.RowInfo = (new Row(ratios, 0, this$.Cursor.X, this$.Cursor.Width));
-    this$.Cursor.Width = (this$.Cursor.Width * Row__get_ActiveRatio(this$.RowInfo));
-}
-
